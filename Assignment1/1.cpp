fb09de704422ca348641167c88c17881082afd7c
JaideepSingh-byte <prince.jaideepsingh@gmail.com>
2026-08-11 19:35:22 +0530

    1

--- Assignment1/1.cpp
+++ Assignment1/1.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 int main()
 {
-    double r,h,s,v;
+    double r,h;
     cout<<"Here I will Tell you the Surface-Area and Volume of a Cylinder of Dimensions that you give us"<<endl<<"Kindly Enter the Dimensions :- "<<endl;
     cout<<"Enter the Radius (r) : ";
     cin>>r;
     cout<<"Enter the Height (h) : ";
     cin>>h;
-
-    s = 2*3.14159*r*h;
-    v = 3.14159*r*r*h;
+    double(s) = 2*3.14159*r*h;
+    double(v) = 3.14159*r*r*h;
     cout<<"The Surface-Area is : "<<s<<endl;
     cout<<"The Volume is : "<<v<<endl;
     
