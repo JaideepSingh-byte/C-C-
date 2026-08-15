fdd55927a863968fedf1c795b8cbc306479c4ac6
JaideepSingh-byte <prince.jaideepsingh@gmail.com>
2026-08-15 20:56:29 +0530

    p

---
+++ Assignment1/2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,c,s;
+cout<<"Enter any three Real Numbers : \n";
+cout << "Number1 : "<<cin >> a;
+cout << "Number2 : "<<cin >> b;
+cout << "Number3 : "<<cin >> c;
+s = a+b+c
+cout << "The sum of the Given Numbers is : " << cin >> (s);
+cout << "And The Average of the Given Numbers is : " << cin >> (s/3);
+    return 0;
+}
