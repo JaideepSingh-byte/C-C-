4a55d41bf405ef15ed12eeac64be66a8bd23ea9a
JaideepSingh-byte <prince.jaideepsingh@gmail.com>
2026-08-16 06:27:22 +0530

    p

--- Assignment1/2.cpp
+++ Assignment1/2.cpp
@@ -2,13 +2,19 @@
 using namespace std;
 int main()
 {
-int a,b,c,s;
-cout<<"Enter any three Real Numbers : \n";
-cout << "Number1 : "<<cin >> a;
-cout << "Number2 : "<<cin >> b;
-cout << "Number3 : "<<cin >> c;
-s = a+b+c
-cout << "The sum of the Given Numbers is : " << cin >> (s);
-cout << "And The Average of the Given Numbers is : " << cin >> (s/3);
+double a;
+double b;
+double c;
+double s;
+cout<<"Enter any three Real Numbers : " << endl;
+cout << "Number1 : ";
+cin >> a;
+cout << "Number2 : ";
+cin >> b;
+cout << "Number3 : ";
+cin >> c;
+s = a+b+c;
+cout << "The sum of the Given Numbers is : "<< s << endl;
+cout << "And The Average of the Given Numbers is : " << (s/3) << endl;
     return 0;
 }
