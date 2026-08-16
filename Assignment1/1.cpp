960637975631ff76661c2d020d7f6995891af397
JaideepSingh-byte <prince.jaideepsingh@gmail.com>
2026-08-16 18:01:32 +0530

    P

--- Assignment1/1.cpp
+++ Assignment1/1.cpp
@@ -3,15 +3,15 @@ using namespace std;
 int main()
 {
     double r,h;
-    cout<<"Here I will Tell you the Surface-Area and Volume of a Cylinder of Dimensions that you give us"<<endl<<"Kindly Enter the Dimensions :- "<<endl;
-    cout<<"Enter the Radius (r) : ";
-    cin>>r;
-    cout<<"Enter the Height (h) : ";
-    cin>>h;
+    cout << "Here I will Tell you the Surface-Area and Volume of a Cylinder of Dimensions that you give us"<<endl<<"Kindly Enter the Dimensions :- "<<endl;
+    cout << "Enter the Radius (r) : ";
+    cin >> r;
+    cout << "Enter the Height (h) : ";
+    cin >> h;
     double(s) = 2*3.14159*r*h;
     double(v) = 3.14159*r*r*h;
-    cout<<"The Surface-Area is : "<<s<<endl;
-    cout<<"The Volume is : "<<v<<endl;
+    cout << "The Surface-Area is : "<<s<<endl;
+    cout << "The Volume is : "<<v<<endl;
     
     return 0;
 }
---
+++ Assignment1/3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+string g,b;
+cout << "Tell me a list of your good and bad traits of your Personality!" << endl;
+cout << "Your Good Trait ";
+cin >> g;
+cout << "Your Bad Trait ";
+cin >> b;
+cout << "Here is the list :- " << endl << "Good Trait : "<< g << endl << "Bad Trait : "<< b << endl;
+    return 0;
+}
