1fadb4655562dd1b7a4d948947d3402a2ba09db6
JaideepSingh-byte <prince.jaideepsingh@gmail.com>
2026-08-16 17:31:31 +0530

    p

---
+++ Assignment1/4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float m;
+    cout << "Enter the Marks you Gain" << endl << "I will tell you the percentage!";
+    cin >> m;
+    if ( m <= 400){
+    float p = m/400.0;
+    cout << "Your percentage is : " << p << endl;
+    }
+    else{
+        cout << "Please Enter a valid input" << endl;
+    }
+    return 0;
+}
