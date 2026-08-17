4f8a10d29e9491b9975917a402a25373afc61255
JaideepSingh-byte <prince.jaideepsingh@gmail.com>
2026-08-17 20:28:18 +0530

    7

--- Assignment1/7.cpp
+++ Assignment1/7.cpp
@@ -5,9 +5,15 @@ int main()
 int n;
 cout << "Enter the Number for the Week Name: " << endl;
 cin >> n;
-switch (n <=7){
-case 0 (n)
-    default (cout << "Today is Sunday\n";)
+switch (n){
+    case 1: cout << "Sunday!\n"; break;
+    case 2: cout << "Monday!\n"; break;
+    case 3: cout << "Tuesday!\n"; break;
+    case 4: cout << "Wednesday!\n"; break;
+    case 5: cout << "Thrusday!\n"; break;
+    case 6: cout << "Friday!\n"; break;
+    case 7: cout << "Saturday!\n"; break;
+        default: cout << "Sunday\n";
 }
     return 0;
 }
