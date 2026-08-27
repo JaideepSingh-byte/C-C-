0a15d39c2ab0bc7d5dc1a8ad5330c36eae576d1f
JaideepSingh-byte <prince.jaideepsingh@gmail.com>
2026-08-27 22:07:46 +0530

    6

--- Assignment2/6.cpp
+++ Assignment2/6.cpp
@@ -4,20 +4,22 @@ int main(){
 char choice;
 do {
     int num;
-
+    bool flag = true;
     cout << "Enter the Positive Number to check wheter it is prime or not: ";
     cin >> num;
     if ( num >= 1){
-        for (int i = 2; i*i <= (num-1) ; i++){
+        for (int i = 2; i*i <= num ; i++){
             if (num%i == 0){
                 cout << "The Number " << num << " is not Prime Number.\n";
+                flag = false;
                 break;
-            } else {
-                cout << "The Number " << num << " is Prime Number.\n";
             }
         }
     } else {
         cout << "Please Enter the valid input!\n";
+    }
+    if (flag == true){
+        cout << "The Number " << num << " is Prime Number.\n";
     }
  cout << "Do you want to continue it!\n" ;
  cout << "If yes, press y" << endl << "If No, press n\n";
