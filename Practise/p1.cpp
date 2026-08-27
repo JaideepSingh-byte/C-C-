d3b93dae602875d0fbc9cd889dc4464f6621683a
JaideepSingh-byte <prince.jaideepsingh@gmail.com>
2026-08-27 18:30:00 +0530

    Practise

--- Practise/p1.cpp
+++ Practise/p1.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
-int main()
-{
- 
+int main(){
+    int n = 1;
+    int sum;
+    while ( n > 0 ){
+        sum = sum + n ;
+        cout << sum << endl;
+        n++;
+    }
     return 0;
 }
