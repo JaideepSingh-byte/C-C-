b9e86f1e25cf89184a4b3072f9421e7412c88e2a
JaideepSingh-byte <prince.jaideepsingh@gmail.com>
2026-08-28 12:54:01 +0530

    5

--- Assignment2/5.cpp
+++ Assignment2/5.cpp
@@ -1,27 +1,43 @@
 #include <iostream>
 using namespace std;
+void billupto100(){
+    cout << "The Electricity Bill amount is: ";
+}
+void billabove100(){
+    cout << "You are charged 15% more of the bill amount, on the basis of your Consumption!\n";
+    cout << "The Total Payable amount is: ";
+}
 int main(){
-char choice;
+    int unit;
 do {
-    int num;
+    cout << "Enter the units consumed by you: ";
+    cin >> unit;
+double bill100,bill200,bill300,total;
+bill100 = unit*(2.5);
+bill200 = (100*2.5) + (unit-100)*3.0;
+bill300 = (100*2.5) + (200*3) + (unit-300)*4.0;
 
-    cout << "Enter the Positive Number to check wheter it is prime or not: ";
-    cin >> num;
-    if ( num >= 1){
-        for (int i = 2; i*i <= (num-1) ; i++){
-            if (num%i == 0){
-                cout << "The Number " << num << " is not Prime Number.\n";
-                break;
-            } else {
-                cout << "The Number " << num << " is Prime Number.\n";
-            }
+    if ((0 <= unit) && (unit <= 80)){
+        billupto100();
+        cout << "200 Rupees only\n";
         }
-    } else {
-        cout << "Please Enter the valid input!\n";
+    else if ((80 < unit) && (unit <= 100)){
+        billupto100();
+        cout << bill100 << endl;
+        }
+    else if ((100 < unit) && (unit <= 300)){
+        total = ((15*bill200)/100);
+        billabove100();
+        cout << bill200 << " + " << total << " = " << (bill200+total) << endl;
+        }
+    else if (300 < unit){
+       total = ((15*bill300)/100);
+        billabove100();
+        cout << bill300 << " + " << total << " = " << (bill300+total) << endl;
+        }
+    else {
+        cout << "Enter a Valid input\n";
     }
- cout << "Do you want to continue it!\n" ;
- cout << "If yes, press y" << endl << "If No, press n\n";
- cin >> choice;
-} while (choice == 'y');
+} while ( unit < 0);
     return 0;
 }
